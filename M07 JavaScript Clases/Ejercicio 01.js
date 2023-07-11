b2a223841575c2af64993ad198e4898509e839ea
amelusin <rodriadri@hotmail.com>
2023-07-11 10:12:54 -0300

    unidad 7

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -17,25 +17,30 @@ function crearUsuario() {
         this.email = email;
         this.password = password;
       }
-  
-      saludar() {
-        return 'Hola, mi nombre es ${this.nombre}.';
+        saludar() {
+            return 'Hola, mi nombre es ' + this.nombre;
+        }
       }
-    }
-  
-    return Usuario;
+   return Usuario;
 }
+
 
 function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function() {
+      return 'Hello World!';
+   }
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function () {
+      return this.split("").reverse().join("")
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
